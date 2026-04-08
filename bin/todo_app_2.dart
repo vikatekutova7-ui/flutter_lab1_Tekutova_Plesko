9adc538c7b67609744109f56218223c2d3f0df6e
s0m1k111 <spavel1207@gmail.com>
2026-04-08 14:20:26 +0300

    dead commit

--- bin/todo_app_2.dart
+++ bin/todo_app_2.dart
@@ -1,4 +1,17 @@
 void main() {
   print('Hello world');
+  String name = 'Artem';
+  String? name2 = null;
+  var count = 0;
+  var title = "Hello";
+  var x = 5;
+  // x = "dsf";
+  final id;
+  id = 5;
+  const app = "Todo";
+  print("$app $id $x $title ${count + 1} $name2 $name");
+  List<String> test = ['язык', 'уроки'];
+  Map<String, dynamic> data = {'key': 1};
+  Set<int> id2 = {1, 2, 3};
   
 }
